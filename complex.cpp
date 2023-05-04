279a4fd0a2ee797f18383126d6ff8d63644d960d
TsalemisIasonas <tsalemisi@gmail.com>
2023-05-05 02:07:16 +0300

    algebric operations

--- complex.cpp
+++ complex.cpp
@@ -2,3 +2,26 @@
 #ifndef CONTEST
 #include "complex.hpp"
 #endif
+
+Complex::Complex(int re, int im):re(re), im(im){}
+
+Complex Complex::add(const Complex za, const Complex zb){
+    return Complex(za.re + zb.re, za.im + zb.im);
+}
+
+Complex Complex::sub(const Complex za, const Complex zb){
+    return Complex(za.re - zb.re, za.im - zb.im);
+}
+
+Complex Complex::mul(const Complex za, const Complex zb){
+    return Complex(za.re * zb.re - za.im * zb.im, za.re * zb.im + zb.re * za.im);
+}
+
+Complex Complex::div(const Complex za, const Complex zb){
+    return Complex((za.re * zb.re + za.im * zb.im) / (zb.re * zb.re - zb.im * zb.im),
+                        (za.im * zb.re - za.re * zb.im) / (zb.re * zb.re - zb.im * zb.im));
+}
+
+int Complex::norm(const Complex z){
+    return sqrt(z.re * z.re + z.im * z.im);
+}
--- complex.hpp
+++ complex.hpp
@@ -4,15 +4,18 @@ class Complex {
         Complex(int re, int im);
         ~Complex();
 
+        Complex add(const Complex za, const Complex zb);
+        Complex sub(const Complex za, const Complex zb);
+        Complex mul(const Complex za, const Complex zb);
+        Complex div(const Complex za, const Complex zb);
+        Complex toPolar(const Complex za, const Complex zb);
+        Complex toCartesian(const Complex za, const Complex zb);
+        int norm(const Complex z);
+
+        friend std::ostream &operator << (std::ostream &out, const Complex &z);
+
     private:
         int re;
         int im;
-
-        Complex addition();
-        Complex subtraction();
-        Complex multiplication();
-        Complex division();
-        Complex polar();
-        Complex cartesian();
 
 };
